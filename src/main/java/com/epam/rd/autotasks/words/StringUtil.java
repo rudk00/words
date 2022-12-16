313374c795be7c58b91c42838588fe6fa0595c5b
Yauheni Rudzko <e-rudko2000@mail.ru>
2022-12-16 11:29:07 +0300

    Operations with string (success)

--- src/main/java/com/epam/rd/autotasks/words/StringUtil.java
+++ src/main/java/com/epam/rd/autotasks/words/StringUtil.java
@@ -1,22 +1,90 @@
 package com.epam.rd.autotasks.words;
 
 import java.util.Arrays;
+import java.util.Objects;
+import java.util.StringJoiner;
 
 public class StringUtil {
-    public static int countEqualIgnoreCaseAndSpaces(String[] words, String sample) {
-        throw new UnsupportedOperationException();
+
+    public static int countEqualIgnoreCaseAndSpaces(String[] words, String sample){
+        int counter = 0;
+        if (words == null || sample == null) {
+            return 0;
+        }
+        for (String word: words) {
+            if (sample.trim().equalsIgnoreCase(word.trim())) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+    public static String[] splitWords(String text){
+        if (text == null) return null;
+        String regEx = "\\W";
+        text = text.replaceAll(regEx, " ").trim();
+        regEx = "\\s+";
+        String[] str = text.split(regEx);
+        if (str[0].isEmpty()) {
+            return null;
+        }
+        return str;
+    }
+    public static String convertPath(String path, boolean toWin){
+        if (path == null || path.isEmpty() || !(isWindowsMatches(path) || isUnixMatches(path))) {
+            return null;
+        }
+        if ((isWindowsMatches(path) && toWin) || (isUnixMatches(path) && !toWin)) {
+            return path;
+        }
+        String resultPath;
+        if (toWin) {
+            resultPath = convertPathToWindows(path);
+        } else {
+            resultPath = convertPathToUnix(path);
+        }
+        return resultPath;
+    }
+    private static boolean isUnixMatches(String path) {
+        return path.matches("([/~])?([\\w.\\-\\s/]*)");
     }
 
-    public static String[] splitWords(String text) {
-        throw new UnsupportedOperationException();
+    private static boolean isWindowsMatches(String path) {
+        return path.matches("(C:\\\\)?([\\w.\\-\\s\\\\]*)");
     }
 
-    public static String convertPath(String path, boolean toWin) {
-        throw new UnsupportedOperationException();
+    private static String convertPathToUnix(String path) {
+        if (path.startsWith("C:\\User")) {
+            path = path.replace("C:\\User", "~");
+        } else if (path.startsWith("C:\\")) {
+            path = path.replace("C:\\", "/");
+        }
+        return path.replace("\\", "/");
     }
 
-    public static String joinWords(String[] words) {
-        throw new UnsupportedOperationException();
+    private static String convertPathToWindows(String path) {
+        if (path.startsWith("~")) {
+            path = path.replace("~", "C:\\User");
+        } else if (path.startsWith("/")) {
+            path = path.replaceFirst("/", "C:\\\\");
+        }
+        return path.replace("/", "\\");
+    }
+    public static String joinWords(String[] words){
+        if (words == null) {
+            return null;
+        }
+        StringBuilder resWord = new StringBuilder();
+        for (String word: words) {
+            resWord.append(word);
+        }
+        if (resWord.length() == 0) return null;
+        StringJoiner joiner = new StringJoiner(", ", "[", "]");
+        for (String word: words) {
+            if (!Objects.equals(word, "")) {
+                joiner.add(word);
+            }
+        }
+        return joiner.toString();
     }
 
     public static void main(String[] args) {
